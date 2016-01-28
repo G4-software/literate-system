0709f1b211d4d7a1b5e82a4174d9236185aa1e69
Sergey Lebedev <lebser305@gmail.com>
2016-01-28 22:41:31 +0400

    # 4
    - Add twig template engine.

---
+++ twig_config.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__."/config.php";
+    require_once __DIR__."/vendor/autoload.php";
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem('templates');
+    $twig = new Twig_Environment($loader,
+        array(
+            'cache'       => 'compilation_cache',
+            'auto_reload' => true
+        ));
